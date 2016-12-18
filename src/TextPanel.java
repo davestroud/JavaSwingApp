fa4e721f4b52386dcfc54179eba716c728ba89db
davestroud <david@davidstroud.me>
2016-12-18 08:38:23 -0600

    deleted unused libraries

--- src/TextPanel.java
+++ src/TextPanel.java
@@ -6,6 +6,10 @@ import javax.swing.JTextArea;
 
 public class TextPanel extends JPanel {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JTextArea textArea;
 
 	public TextPanel() {
